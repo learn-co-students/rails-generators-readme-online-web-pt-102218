c86153bce85955b83709876c84a98f55be17d8d1
Matthew Sherman <msherman0801@gmail.com>
2019-03-20 20:14:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ActiveRecord::Base
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(params.require(:post).permit(:title, :description, :post_status))
+        redirect_to post_path(@post)
+    end
+
+
 end
